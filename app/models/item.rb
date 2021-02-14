6ef72134279e88a64607ad428ef00c4000222713
rei1129 <freead9211@yahoo.co.jp>
2021-02-14 17:54:11 +0900

    4th commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,6 +8,7 @@ class Item < ApplicationRecord
     validates :name
     validates :text
     validates :image
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
     with_options numericality: { other_than: 1 } do
@@ -17,7 +18,6 @@ class Item < ApplicationRecord
       validates :prefecture_id
       validates :scheduled_delivery_id
     end
-    validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :sales_status, :shipping_fee_status, :prefecture, :scheduled_delivery
