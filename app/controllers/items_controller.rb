8dc1f666335a563dc3e1e648f51e0abf2bfc7815
rei1129 <freead9211@yahoo.co.jp>
2021-02-20 12:44:03 +0900

    指摘箇所の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -35,9 +35,12 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-     @item = Item.find(params[:id])
+    if current_user.id == @item.user_id
      @item.destroy
      redirect_to root_path
+    else
+      redirect_to item_path
+    end
   end
 
   private
